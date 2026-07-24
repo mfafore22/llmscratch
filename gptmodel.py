9ba17f4fc4c247059ba3438e2cf584fe02a9171d
mfafore22 <faforemichael001@gmail.com>
2026-07-24 23:42:28 +0300

    I adjusted the gpt model , and remove dummy layernorm and transformer block , wrote real one and called it in the gpt model which was a dummy

--- gptmodel.py
+++ gptmodel.py
@@ -1,22 +1,24 @@
 import torch
 import torch.nn as nn
+from transformerblock import TransformerBlock
+from  layernorm import LayerNorm
 
-class DummyGPTModel(nn.Module):
+class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
         self.trf_blocks = nn.Sequential(
-            *[DummyTransformerBlock(cfg)
+            *[TransformerBlock(cfg)
               for _ in range(cfg["n_layers"])]
         )
-        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(
             cfg["emb_dim", cfg["vocab_size", bias=False]]
         )
     
-    def forwward(self, in_idx):
+    def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx)
         pos_embeds = self.pos_emb(
@@ -29,17 +31,3 @@ class DummyGPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
     
-
-class DummyTransformerBlock(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-    
-    def forward(Self, x):
-        return x
-
-class DummyLayerNorm(nn.Module):
-    def __init__(self, normalized_shape, eps=1e-5):
-        super().__init__()
-
-        def forward(self, x):
-            return x
