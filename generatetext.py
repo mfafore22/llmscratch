3b0f629cd4bc48940f324eaf057df59960bcc0b3
mfafore22 <faforemichael001@gmail.com>
2026-07-28 18:53:36 +0300

    generate text

---
+++ generatetext.py
@@ -0,0 +1,41 @@
+import torch
+import torch as nn
+import tokenizer
+from gptmodel import model
+from gptconfig import GPT_CONFIG_124M
+
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+start_context = "Hello. I am"
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+
+"""
+eval mode this disbales random components like dropout"""
+
+model.eval()
+out = generate_text_simple(
+    model = model,
+    idx = encoded_tensor,
+    max_new_tokens = 6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output:", out)
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
