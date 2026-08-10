282cbf2667b2e9bb0b189c8b3f9beefc56f48fc0
mfafore22 <faforemichael001@gmail.com>
2026-08-10 18:30:19 +0300

    added loss function

--- exampledeepneuralnetwork.py
+++ exampledeepneuralnetwork.py
@@ -22,3 +22,17 @@ class ExampleDeepNeuralNetwork(nn.Module):
             else:
                 x = layer_output
         return x 
+
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean()}")
