69cd63c1e7109caec807594d16d2fe23b673c0c5
mfafore22 <faforemichael001@gmail.com>
2026-08-11 13:56:36 +0300

    adjusted some files

--- exampledeepneuralnetwork.py
+++ exampledeepneuralnetwork.py
@@ -24,15 +24,5 @@ class ExampleDeepNeuralNetwork(nn.Module):
         return x 
 
 
-def print_gradients(model, x):
-    output = model(x)
-    target = torch.tensor([[0.]])
 
-    loss = nn.MSELoss()
-    loss = loss(output, target)
 
-    loss.backward()
-
-    for name, param in model.named_parameters():
-        if 'weight' in name:
-            print(f"{name} has gradient mean of {param.grad.abs().mean()}")
---
+++ printgradients.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean()}")
