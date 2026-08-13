cd30c608f8aa4a130497c6ac357f9741231314e9
mfafore22 <faforemichael001@gmail.com>
2026-08-13 22:38:56 +0300

    text

---
+++ loadfile.py
@@ -0,0 +1,10 @@
+import tokenizer
+file_path = "the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+total_characters = len(text_data)
+total_tokens = len(tokenizer.encode(text_data))
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
---
+++ texttotoken.py
@@ -0,0 +1,26 @@
+import tiktoken
+import torch
+from generatetext import generate_text_simple
+from gpttextgeneration import model
+from gptconfig import GPT_CONFIG_124M
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|}>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding('gpt2')
+
+token_ids = generate_text_simple(
+    model = model,
+    idx=text_to_token_ids(start_context, tokenizer),
+    max_new_tokens = 10,
+    context_size= GPT_CONFIG_124M["context_length"]
+
+)
