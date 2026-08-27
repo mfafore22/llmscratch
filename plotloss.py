3593b35a55835ba19f5da0cb1ff8285f4f1bd509
mfafore22 <faforemichael001@gmail.com>
2026-08-27 22:41:12 +0300

    adjusted plot loss

--- plotloss.py
+++ plotloss.py
@@ -1,5 +1,8 @@
+import torch
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+
+
 def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     fig, ax1 = plt.subplots(figsize=(5, 3))
     ax1.plot(epochs_seen, train_losses, label="Trainig loss")
@@ -14,5 +17,29 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     fig.tight_layout()
     plt.show()
 
-epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
-plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+if __name__ == "__main__":
+    from gptmodel import GPTModel
+    from gptconfig import GPT_CONFIG_124M
+    from traindata import train_loader, val_loader
+    from tokenizer import tokenizer
+    from trainmodel import train_model_simple
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.to(device)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=0.0004, weight_decay=0.1
+    )
+
+    num_epochs = 10
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+        start_context="Every effort moves you", tokenizer=tokenizer,
+    )
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
